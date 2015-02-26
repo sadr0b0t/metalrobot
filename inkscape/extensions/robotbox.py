4416398f8a7e0d89012097c366e9b1989cead73d
1i7 <ii1i7@yandex.ru>
2015-02-26 21:21:35 +0300

    Переработать конструкцию (всякие отступы) с учетом толщины картона

--- inkscape/extensions/robotbox.py
+++ inkscape/extensions/robotbox.py
@@ -39,11 +39,15 @@ class RobotBox(inkex.Effect):
         thickness  = self.unittouu( str(self.options.thickness) + self.options.unit )
 
         # small ears (to be hidden inside the box borders) length
-        ear1 = depth * 2 / 3
+        ear1 = height / 3
 
         # big ears skew = ~25 degrees
         # skew_shift = depth*2/3 * tg(25)
         skew_shift = depth*2/3 * 0.47
+
+        # inner width of the box (without left and right slots),
+        # use it just for convenience
+        width2 = width-thickness*4
 
         # Generate box points
         # Details on the shape here:
@@ -53,113 +57,122 @@ class RobotBox(inkex.Effect):
             [ 'M', [
                 # start from left bottom "ear" and go left and up
                 # ear 1
-                0, 0,    -ear1, 0, 
-                -ear1, depth,    0, depth, 
+                0,0,    -ear1,0, 
+                -ear1,depth-thickness*2,    0,depth-thickness*2, 
                 # ear 2
-                0, depth+thickness,    -depth, depth+thickness,    -depth-thickness*3, depth+thickness,    -depth-thickness*3-depth, depth+thickness,
-                -depth-thickness*3-depth, depth+thickness+height,    -depth-thickness*3, depth+thickness+height,    -depth, depth+thickness+height,    0, depth+thickness+height,
+                0,depth,    -thickness*2,depth,    -thickness*2-depth,depth,    -thickness*2-depth-thickness*4,depth,    
+                -thickness*2-depth-thickness*4-depth+thickness,depth,
+                
+                -thickness*2-depth-thickness*4-depth+thickness,depth+height-thickness*2,    -thickness*2-depth-thickness*4,depth+height-thickness*2,    
+                -thickness*2-depth,depth+height-thickness*2,    -thickness*2,depth+height-thickness*2,    0,depth+height-thickness*2,
                 # ear 3
-                0, depth+thickness+height+thickness,    -ear1, depth+thickness+height+thickness, 
-                -ear1, depth+thickness+height+thickness+depth,    thickness, depth+thickness+height+thickness+depth,
+                0,depth+height,    -ear1,depth+height,
+                -ear1,depth+height+depth-thickness*2,    thickness*2,depth+height+depth-thickness*2,
                 # ear 4
-                thickness, depth+thickness+height+thickness+depth+thickness,    thickness-depth*2/3, depth+thickness+height+thickness+depth+thickness+skew_shift,
-                thickness-depth*2/3, depth+thickness+height+thickness+depth+thickness+height-skew_shift,    thickness, depth+thickness+height+thickness+depth+thickness+height
+                thickness*2,depth+height+depth,    thickness*2-depth*2/3,depth+height+depth+skew_shift,
+                thickness*2-depth*2/3,depth+height+depth+height-thickness*2-skew_shift,    thickness*2,depth+height+depth+height-thickness*2
             ] ],
             # ear 5: manual shape (drawn for 62x38x23 box), converted to proportion based on depth value
             # m 0,0 c -39.88719,-0.7697 -90.44391,-0.7593 -73.26685,35.3985 11.37507,22.1855 33.21015,45.182 73.26685,46.0975 z
             [ 'L', [
-                thickness, depth+thickness+height+thickness+depth+thickness+height+thickness, 0, depth+thickness+height+thickness+depth+thickness+height+thickness
+                thickness*2,depth+height+depth+height+thickness,    -thickness,depth+height+depth+height+thickness
             ] ],
             [ 'C', [
-#                -39.88719, depth+thickness+height+thickness+depth+thickness+height+thickness-0.7697,
-#                -90.44391, depth+thickness+height+thickness+depth+thickness+height+thickness-0.7593,
-#                -73.26685, depth+thickness+height+thickness+depth+thickness+height+thickness+35.3985,
-#                -73.26685+11.37507, depth+thickness+height+thickness+depth+thickness+height+thickness+35.3985+22.1855,
-#                -73.26685+33.21015, depth+thickness+height+thickness+depth+thickness+height+thickness+35.3985+45.182,
+#                -39.88719,          depth+height+depth+height+thickness-0.7697,
+#                -90.44391,          depth+height+depth+height+thickness-0.7593,
+#                -73.26685,          depth+height+depth+height+thickness+35.3985,
+#                -73.26685+11.37507, depth+height+depth+height+thickness+35.3985+22.1855,
+#                -73.26685+33.21015, depth+height+depth+height+thickness+35.3985+45.182,
 
-                -depth/2, depth+thickness+height+thickness+depth+thickness+height+thickness,
-                -depth/10*11, depth+thickness+height+thickness+depth+thickness+height+thickness,
-                -depth/8*7, depth+thickness+height+thickness+depth+thickness+height+thickness+depth/16*7,
-                -depth/8*6, depth+thickness+height+thickness+depth+thickness+height+thickness+depth/16*11,
-                -depth/2, depth+thickness+height+thickness+depth+thickness+height+thickness+depth,
-                0, depth+thickness+height+thickness+depth+thickness+height+thickness+depth
-
+                -thickness-(depth-thickness*2)/2,     depth+height+depth+height+thickness,
+                -thickness-(depth-thickness*2)/10*11, depth+height+depth+height+thickness,
+                -thickness-(depth-thickness*2)/8*7,   depth+height+depth+height+thickness+(depth-thickness*2)/16*7,
+                -thickness-(depth-thickness*2)/8*6,   depth+height+depth+height+thickness+(depth-thickness*2)/16*11,
+                -thickness-(depth-thickness*2)/2,     depth+height+depth+height+thickness+(depth-thickness*2),
+                -thickness,                           depth+height+depth+height+thickness+(depth-thickness*2)
             ] ],
             # now go to the right and go down in reverse order
             # ear 6: manual shape (drawn for 62x38x23 box), converted to proportion based on depth value
-            # m 0,0 c 17.177,-36.1578 -33.3797,-36.1682 -73.2847,-35.3867 l 0,81.4958 c 40.0746,-0.9273 61.9096,-23.9238 73.2847,-46.1093 z
             [ 'L', [
-                width, depth+thickness+height+thickness+depth+thickness+height+thickness+depth
+                width2+thickness,depth+height+depth+height+thickness+(depth-thickness*2)
             ] ],
             [ 'C', [
-#                width+40.0746, depth+thickness+height+thickness+depth+thickness+height+thickness+depth-0.9273,
-#                width+61.9096, depth+thickness+height+thickness+depth+thickness+height+thickness+depth-23.9238,
-#                width+73.2847, depth+thickness+height+thickness+depth+thickness+height+thickness+depth-46.1093,
-#                width+73.2847+17.177, depth+thickness+height+thickness+depth+thickness+height+thickness+depth-46.1093-36.1578,
-#                width+73.2847-33.3797, depth+thickness+height+thickness+depth+thickness+height+thickness+depth-46.1093-36.1682,
-
-                width+depth/2, depth+thickness+height+thickness+depth+thickness+height+thickness+depth,
-                width+depth/8*6, depth+thickness+height+thickness+depth+thickness+height+thickness+depth/16*11,
-                width+depth/8*7, depth+thickness+height+thickness+depth+thickness+height+thickness+depth/16*7,
-                width+depth/10*11, depth+thickness+height+thickness+depth+thickness+height+thickness,
-                width+depth/2, depth+thickness+height+thickness+depth+thickness+height+thickness,
-                width, depth+thickness+height+thickness+depth+thickness+height+thickness
+                width2+thickness+(depth-thickness*2)/2,     depth+height+depth+height+thickness+(depth-thickness*2),
+                width2+thickness+(depth-thickness*2)/8*6,   depth+height+depth+height+thickness+(depth-thickness*2)/16*11,
+                width2+thickness+(depth-thickness*2)/8*7,   depth+height+depth+height+thickness+(depth-thickness*2)/16*7,
+                width2+thickness+(depth-thickness*2)/10*11, depth+height+depth+height+thickness,
+                width2+thickness+(depth-thickness*2)/2,     depth+height+depth+height+thickness,
+                width2+thickness,                           depth+height+depth+height+thickness
             ] ],
             [ 'L', [
-                width-thickness, depth+thickness+height+thickness+depth+thickness+height+thickness
+                width2-thickness*2,depth+height+depth+height+thickness
             ] ],
             # ear 7    
             [ 'L', [
-                width-thickness, depth+thickness+height+thickness+depth+thickness+height,    width-thickness+depth*2/3, depth+thickness+height+thickness+depth+thickness+height-skew_shift,
-                width-thickness+depth*2/3, depth+thickness+height+thickness+depth+thickness+skew_shift,    width-thickness, depth+thickness+height+thickness+depth+thickness,
+                width2-thickness*2,depth+height+depth+height-thickness*2,    width2-thickness*2+depth*2/3,depth+height+depth+height-thickness*2-skew_shift,
+                width2-thickness*2+depth*2/3,depth+height+depth+skew_shift,    width2-thickness*2,depth+height+depth,
                 # ear 8
-                width-thickness, depth+thickness+height+thickness+depth,    width+ear1, depth+thickness+height+thickness+depth,
-                width+ear1, depth+thickness+height+thickness,    width, depth+thickness+height+thickness,
+                width2-thickness*2,depth+height+depth-thickness*2,    width2+ear1,depth+height+depth-thickness*2,
+                width2+ear1,depth+height,    width2,depth+height,
                 # ear 9
-                width, depth+thickness+height,    width+depth, depth+thickness+height,    width+depth+thickness*3, depth+thickness+height,    width+depth+thickness*3+depth, depth+thickness+height,
-                width+depth+thickness*3+depth, depth+thickness,    width+depth+thickness*3, depth+thickness,    width+depth, depth+thickness,    width, depth+thickness,
+                width2,depth+height-thickness*2,    width2+thickness*2,depth+height-thickness*2,    width2+thickness*2+depth,depth+height-thickness*2,    
+                width2+thickness*2+depth+thickness*4,depth+height-thickness*2,    width2+thickness*2+depth+thickness*4+depth-thickness,depth+height-thickness*2,
+
+                width2+thickness*2+depth+thickness*4+depth-thickness,depth,    width2+thickness*2+depth+thickness*4,depth,    width2+thickness*2+depth,depth,    
+                width2+thickness*2,depth,    width2,depth,
                 # ear 10
-                width, depth,    width+ear1, depth,
-                width+ear1, 0,    width, 0
+                width2,depth-thickness*2,    width2+ear1,depth-thickness*2,
+                width2+ear1,0,    width2,0
             ] ],
             [ 'Z', [] ]
         ]
 
         # vertical bends
-        bend_line_v11 = [ [ 'M', [ 0, 0, 
-            0, depth+thickness+height+thickness+depth ] ] ]
-        bend_line_v12 = [ [ 'M', [ thickness, depth+thickness+height+thickness+depth+thickness, 
-            thickness, depth+thickness+height+thickness+depth+thickness+height ] ] ]
-        bend_line_v13 = [ [ 'M', [ 0, depth+thickness+height+thickness+depth+thickness+height+thickness, 
-            0, depth+thickness+height+thickness+depth+thickness+height+thickness+depth ] ] ]
-        
-        bend_line_v21 = [ [ 'M', [ width, 0, 
-            width, depth+thickness+height+thickness+depth ] ] ]
-        bend_line_v22 = [ [ 'M', [ width-thickness, depth+thickness+height+thickness+depth+thickness, 
-            width-thickness, depth+thickness+height+thickness+depth+thickness+height ] ] ]
-        bend_line_v23 = [ [ 'M', [ width, depth+thickness+height+thickness+depth+thickness+height+thickness, 
-            width, depth+thickness+height+thickness+depth+thickness+height+thickness+depth ] ] ]
-        
-        
-        bend_line_v14 = [ [ 'M', [ -depth-thickness/2, depth+thickness, 
-            -depth-thickness/2, depth+thickness+height ] ] ]
-        bend_line_v15 = [ [ 'M', [ -depth-thickness*3-thickness/2, depth+thickness, 
-            -depth-thickness*3-thickness/2, depth+thickness+height ] ] ]
+        # left
+        bend_line_vl1 = [ [ 'M', [ 0, 0, 
+            0, depth-thickness*2 ] ] ]
+        bend_line_vl2 = [ [ 'M', [ -thickness*2, depth, 
+            -thickness*2, depth+height-thickness*2 ] ] ]
+        bend_line_vl3 = [ [ 'M', [ 0, depth+height, 
+            0, depth+height+depth-thickness*2 ] ] ]
+        bend_line_vl4 = [ [ 'M', [ thickness*2, depth+height+depth, 
+            thickness*2, depth+height+depth+height-thickness*2 ] ] ]
+        bend_line_vl5 = [ [ 'M', [ -thickness, depth+height+depth+height+thickness, 
+            -thickness, depth+height+depth+height+thickness+depth-thickness*2 ] ] ]
 
-        bend_line_v24 = [ [ 'M', [ width+depth+thickness/2, depth+thickness, 
-            width+depth+thickness/2, depth+thickness+height ] ] ]
-        bend_line_v25 = [ [ 'M', [ width+depth+thickness*3+thickness/2, depth+thickness, 
-            width+depth+thickness*3+thickness/2, depth+thickness+height ] ] ]
+            
+        bend_line_vl6 = [ [ 'M', [ -thickness*2-depth, depth, 
+            -thickness*2-depth, depth+height-thickness*2 ] ] ]
+        bend_line_vl7 = [ [ 'M', [ -thickness*2-depth-thickness*4, depth, 
+            -thickness*2-depth-thickness*4, depth+height-thickness*2 ] ] ]
+            
+        # right
+        bend_line_vr1 = [ [ 'M', [ width2, 0, 
+            width2, depth-thickness*2 ] ] ]
+        bend_line_vr2 = [ [ 'M', [ width2+thickness*2, depth, 
+            width2+thickness*2, depth+height-thickness*2 ] ] ]
+        bend_line_vr3 = [ [ 'M', [ width2, depth+height, 
+            width2, depth+height+depth-thickness*2 ] ] ]
+        bend_line_vr4 = [ [ 'M', [ width2-thickness*2, depth+height+depth, 
+            width2-thickness*2, depth+height+depth+height-thickness*2 ] ] ]
+        bend_line_vr5 = [ [ 'M', [ width2+thickness, depth+height+depth+height+thickness, 
+            width2+thickness, depth+height+depth+height+thickness+depth-thickness*2 ] ] ]
+        
+
+        bend_line_vr6 = [ [ 'M', [ width2+thickness*2+depth, depth, 
+            width2+thickness*2+depth, depth+height-thickness*2 ] ] ]
+        bend_line_vr7 = [ [ 'M', [ width2+thickness*2+depth+thickness*4, depth, 
+            width2+thickness*2+depth+thickness*4, depth+height-thickness*2 ] ] ]
         
         # horizontal bends
-        bend_line_h1 = [ [ 'M', [ 0, depth+thickness/2, 
-            width, depth+thickness/2 ] ] ]
-        bend_line_h2 = [ [ 'M', [ 0, depth+thickness+height+thickness/2, 
-            width, depth+thickness+height+thickness/2 ] ] ]
-        bend_line_h3 = [ [ 'M', [ thickness, depth+thickness+height+thickness+depth+thickness/2, 
-            width-thickness, depth+thickness+height+thickness+depth+thickness/2 ] ] ]
-        bend_line_h4 = [ [ 'M', [ thickness, depth+thickness+height+thickness+depth+thickness+height+thickness/2, 
-            width-thickness, depth+thickness+height+thickness+depth+thickness+height+thickness/2 ] ] ]
+        bend_line_h1 = [ [ 'M', [ 0, depth-thickness, 
+            width2, depth-thickness ] ] ]
+        bend_line_h2 = [ [ 'M', [ 0, depth+height-thickness, 
+            width2, depth+height-thickness ] ] ]
+        bend_line_h3 = [ [ 'M', [ thickness*2, depth+height+depth-thickness, 
+            width2-thickness*2, depth+height+depth-thickness ] ] ]
+        bend_line_h4 = [ [ 'M', [ thickness*2, depth+height+depth+height, 
+            width2-thickness*2, depth+height+depth+height ] ] ]
 
 
         # Embed drawing in group to make animation easier:
@@ -176,37 +189,52 @@ class RobotBox(inkex.Effect):
         # Create SVG Paths for bend lines
         # draw bend lines with blue
         style = { 'stroke': '#44aaff', 'fill': 'none' }
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v11 )}
+
+        # left
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vl1 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v12 )}
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vl2 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v13 )}
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vl3 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v21 )}
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vl4 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v22 )}
-        inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
-
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v23 )}
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vl5 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
         
-
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v14 )}
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vl6 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v15 )}
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vl7 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v24 )}
+        # right
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vr1 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
-        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_v25 )}
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vr2 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vr3 )}
+        inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
+
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vr4 )}
+        inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
+
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vr5 )}
+        inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
+
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vr6 )}
+        inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
+
+        path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_vr7 )}
+        inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
+
+        # horizontal
         path_attribs = {'style':simplestyle.formatStyle(style), 'd':formatPath( bend_line_h1 )}
         inkex.etree.SubElement(g, inkex.addNS('path','svg'), path_attribs )
 
