9dc745f9c8d48b92415061c8481e88cbc43ca3a8
1i7 <ii1i7@yandex.ru>
2016-06-16 15:00:42 +0300

    Первая версия инструкции по подключению проводов

---
+++ doc/robotcar-model2-инструкция/inkscape-schema/instr-pages/export-pdf/merge-pdfs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Склеить страницы в один файл
+pdfunite 01-connect-motors.pdf 02-connect-inputs.pdf 03-connect-en.pdf 04-combine-ext-GND.pdf 05-conect-GND.pdf 06-connect-VSS.pdf 07-connect-VS.pdf robotcar-wiring.pdf
+
