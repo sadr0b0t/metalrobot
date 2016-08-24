6dfe73280ae706f4ba9dad423bc4720e4e7cf9b0
1i7 <ii1i7@yandex.ru>
2016-08-24 22:43:36 +0300

    Инструкция по подключению моторов через L293D

---
+++ doc/robotcar-model2-инструкция-провода/instr-pages-portrait/export-pdf/merge-pdfs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Склеить страницы в один файл
+pdfunite 00-info.pdf \
+    01-connect-motors.pdf \
+    02-connect-inputs.pdf \
+    03-connect-en.pdf \
+    04-combine-ext-GND.pdf \
+    05-conect-GND.pdf \
+    06-connect-VSS.pdf \
+    07-connect-VS.pdf \
+    robotcar-wiring-port.pdf
+
